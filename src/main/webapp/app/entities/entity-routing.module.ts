35daa6f884094568a609648d6b8376c70e98e8cd
Dawid Luczak <dawid.luczak.it@gmail.com>
2022-01-04 12:47:25 +0100

    app modules

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,7 +4,31 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+      {
+        path: 'department',
+        data: { pageTitle: 'eventloggerApp.department.home.title' },
+        loadChildren: (): Promise<any> => import('./department/department.module').then(m => m.DepartmentModule),
+      },
+      {
+        path: 'schedule',
+        data: { pageTitle: 'eventloggerApp.schedule.home.title' },
+        loadChildren: (): Promise<any> => import('./schedule/schedule.module').then(m => m.ScheduleModule),
+      },
+      {
+        path: 'event',
+        data: { pageTitle: 'eventloggerApp.event.home.title' },
+        loadChildren: (): Promise<any> => import('./event/event.module').then(m => m.EventModule),
+      },
+      {
+        path: 'employee',
+        data: { pageTitle: 'eventloggerApp.employee.home.title' },
+        loadChildren: (): Promise<any> => import('./employee/employee.module').then(m => m.EmployeeModule),
+      },
+      {
+        path: 'community',
+        data: { pageTitle: 'eventloggerApp.community.home.title' },
+        loadChildren: (): Promise<any> => import('./community/community.module').then(m => m.CommunityModule),
+      },
     ]),
   ],
 })
