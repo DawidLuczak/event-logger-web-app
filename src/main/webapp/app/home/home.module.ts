ae9dd3860b78f6a43a266c8cbd89d77d026f685f
Dawid Luczak <dawid.luczak.it@gmail.com>
2022-01-04 12:43:37 +0100

    home component

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { BannerModule } from 'app/layouts/banner/banner.module';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [BannerModule, SharedModule, RouterModule.forChild([HOME_ROUTE])],
   declarations: [HomeComponent],
+  exports: [HomeComponent],
 })
 export class HomeModule {}
