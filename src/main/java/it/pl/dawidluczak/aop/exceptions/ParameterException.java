164dce34b48cf4cb4751104f5e96302d652c72fa
Dawid Luczak <dawid.luczak.it@gmail.com>
2022-01-04 12:31:44 +0100

    exceptions - parameter & notImplemented

---
+++ src/main/java/it/pl/dawidluczak/aop/exceptions/ParameterException.java
@@ -0,0 +1,10 @@
+package it.pl.dawidluczak.aop.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.parameter")
+public class ParameterException extends RuntimeException {
+
+    private static final long serialVersionUID = 4627089149355905161L;
+}
